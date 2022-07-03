27c4de203f04b4302a791fd5f8695075d9addb3e
Raginda Firdaus <97087766+raginda-firdaus@users.noreply.github.com>
2022-07-03 15:24:50 +0700

    add get bookmark by id & delete bookmark by id

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 import validators
-from src.constants.http_status_code import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
+from src.constants.http_status_code import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 from src.database import Bookmark, db
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
@@ -70,3 +70,44 @@ def handle_bookmarks():
             'data': data,
             'meta': meta
             }), HTTP_200_OK
+
+@bookmarks.get('/<int:id>')
+@jwt_required()
+def get_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({
+            'msg': 'Item not found!'
+        }), HTTP_404_NOT_FOUND
+
+    return jsonify({
+        'id': bookmark.id,
+        'url': bookmark.url,
+        'short_url': bookmark.short_url,
+        'visit': bookmark.visits,
+        'body': bookmark.body,
+        'created_at': bookmark.created_at,
+        'updated_at': bookmark.updated_at,
+    }), HTTP_200_OK
+
+@bookmarks.delete('/<int:id>')
+@jwt_required()
+def delete_bookmark(id):
+    current_user = get_jwt_identity()
+
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({
+            'msg': 'Item not found!'
+        }), HTTP_404_NOT_FOUND
+
+    db.session.delete(bookmark)
+    db.session.commit()
+
+    return ({}), HTTP_204_NO_CONTENT
+
+
